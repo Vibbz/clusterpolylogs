b79dbd40b6d3dbda60e458e1251a50cf45eba8b4
Vibbz <109175366+Vibbz@users.noreply.github.com>
2023-07-06 16:24:04 -0400

    Update and rename x_and_a_coordinates.py to main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import sys
 from time import time
 from time import sleep
 from functions import mutation, quiver_mutations, coordinates, main
+from quivers import generate_quiver
 
 #Here we consider quivers as nxn skew-symmetric matrices where the entries e_ij correspond to the number of edges going from vertex i into vertex j, with the number considered negative if the direction is reversed. Mutation is described on quivers in this manner in equation (1.1) at the bottom of the first page on Zickert's notes on Cluster Algebras.
 
@@ -21,7 +22,6 @@ gr_36_vertices_s=[Symbol(x) for x in gr_36_vertices]
 #dummy_matrix=np.array([[1,2,3,4,5,6],[7,8,9,10,11,12],[13,14,16,17,18,19]])
 
 dummy_matrix=np.array([[n+6*i+random() for n in range(6)] for i in range(3)])
-#print(dummy_matrix)
 
 
 #These are the associated nubmers to each vertex as listed above
@@ -31,9 +31,7 @@ gr_36_vertices_num=[np.linalg.det(np.array([ [row[int(i)-1] for i in list(plucke
 #Later, I'll make code that actually just generates this stuff in general.
 
 
-#####
 # Below is the main program
-#####
 
 
 
@@ -56,15 +54,21 @@ while True:
     if option1=='co':
       option1='gr'
   
-  quiver=input('Enter the quiver to use.\n gr36 uses the quiver for gr(3,6) with plucker coordinate vertices. \n ex2 uses the quiver 1->2 with default vertex labeling \n ex3 uses the quiver 1->2->3 with default vertex labeling. \n Enter: ')
+  quiver_data_s=input('Enter the quiver to use.\n gr36 uses the quiver for gr(3,6) with plucker coordinate vertices. \n gr2n uses the quiver for gr(2,n) with plucker coordinate vertices. \n ex2 uses the quiver 1->2 with default vertex labeling. \n ex3 uses the quiver 1->2->3 with default vertex labeling. \n Enter: ')
+  quiver_data=generate_quiver(quiver_data_s)
+  
   if option1=='gr':
-    find_coords=input('Enter "x" to find X-coords and "a" to find A-coords: ')
+    find_coords=input('Enter "x" to find X-coords, "a" to find A-coords, or "cl" to find all the clusters: ')
     if find_coords=="x":
       do_find_x_coords=1
       do_find_a_coords=0
     else:
       do_find_a_coords=1
       do_find_x_coords=0
+    if find_coords=="cl":
+      do_find_clusters=1
+    else:
+      do_find_clusters=0
   else:
     do_find_a_coords=0
     do_find_x_coords=0
@@ -82,13 +86,26 @@ while True:
   
   final_result=set()
   iter=0
+  
   try:
     while condition==True:
       iter+=1
-  
+
+      if test_mode==True:
+        print('\n General test mode output...')
+        print('Option 1: {0}'.format(option1))
+        print('Number of mutations: {0}'.format(num_of_mutations))
+        print('Quiver: ',quiver_data[0])
+        print('Vertices: ',quiver_data[1])
+        print('Mutables: ',quiver_data[2])
+        print('Numerical vertices: ', quiver_data[3])
+        print()
+
+      
       ####################### 
       #Actually calling main#
-      new_results=main(option1,num_of_mutations,quiver,do_find_x_coords,do_find_a_coords,test_mode)
+      new_results=main(option1,num_of_mutations,[quiver_data[0].copy(),quiver_data[1].copy(),quiver_data[2],quiver_data[3].copy()],do_find_x_coords,do_find_a_coords,do_find_clusters,test_mode)
+      
       old_length=len(final_result)
       
       for co in new_results:
@@ -96,11 +113,19 @@ while True:
       
       if option1=='gr':
         #Numeric test for equality modulo plucker relations
-        if quiver=='gr36':
+        #if statement is standin
+        
+        vertices_numerical=quiver_data[3].copy()
+        vertices=[Symbol(x) for x in quiver_data[1]]
+
+        
+        if True==True:
           try:
             to_remove=[]
             pairs_of_dupes_found=0
-            evals_plucker=[expr.subs([z for z in zip(gr_36_vertices_s,gr_36_vertices_num)]) for expr in final_result]
+            
+            evals_plucker=[expr.subs([z for z in zip(vertices,vertices_numerical)]) for expr in final_result]
+            
             for j in range(len(evals_plucker)):
               for i in range(j,len(evals_plucker)):
                   if j!=i:
@@ -108,8 +133,8 @@ while True:
                       pairs_of_dupes_found+=1
   
                       #Testmode information
-                      if test_mode==True:
-                        print('\n Testmode output, duplicates: \n', i, j, list(final_result)[i],list(final_result)[j], '\n', list(final_result)[i].subs([z for z in zip(gr_36_vertices_s,gr_36_vertices_num)]), list(final_result)[j].subs([z for z in zip(gr_36_vertices_s,gr_36_vertices_num)]),evals_plucker[i],evals_plucker[j])
+                      #if test_mode==True:
+                      #  print('\n Test mode output, finding duplicates: \n', i, j, list(final_result)[i],list(final_result)[j], '\n', list(final_result)[i].subs([z for z in zip(vertices,vertices_numerical)]), list(final_result)[j].subs([z for z in zip(vertices,vertices_numerical)]),evals_plucker[i],evals_plucker[j])
                       #####################
                       to_remove+=[list(final_result)[j]]
   
@@ -117,11 +142,12 @@ while True:
               print('Test dupes = amt removed passed')
             elif test_mode==True:
               print('Test dupes = amt rmoved failed', pairs_of_dupes_found, len(to_remove))
+
             for x in to_remove:
               final_result.remove(x)
             final_result=set(final_result)
-          except IndexError:
-            print('\n Error in numeric test \n')
+          except:
+            print('\n Error in numeric test, or vertices are not plucker coordinates.  \n')
             pass
             
           sys.stdout.write('\r \n {1}: Removed {4} duplicates. Found {3} new {2}-coordinates. Found {0} total {2}-coordinates. \n'.format(len(final_result),iter,find_coords.capitalize(),-old_length+len(final_result),len(to_remove)))
@@ -144,7 +170,7 @@ while True:
   
   #Test sameness of coordinates
   if option1=='gr':  
-    option3=input('Enter "y" to test equivalency among X-coordinates symbolically: ')
+    option3=input('Enter "y" to test equivalency among {0}-coordinates symbolically: '.format(find_coords))
     if option3=='y':
           final_result=list(final_result)
           #print(final_result[len(final_result)-1])
@@ -193,3 +219,4 @@ while True:
       print('\n',test)
   
   print('\n Run again:')
+
